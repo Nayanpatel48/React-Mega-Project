0011eea9aff570ba9b9cd4e1cfc902f334103d1f
nayan <www.patelnayan187@gmail.com>
2025-02-09 19:31:22 +0530

    configration done

--- React-Mega-Project/src/appwrite/config.js
+++ React-Mega-Project/src/appwrite/config.js
@@ -77,6 +77,54 @@ export class Service{
             return false;
         }
     }
+
+    async getPost(queries= [Query.equal('status', 'active')]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("getPost Error..")
+            return false
+        }
+    }
+
+    //file upload method/service
+    async uploadFile(file){
+        try {
+            return await this.storage.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("upload file error")
+            return true;
+        }
+    }
+
+    //delete file
+    async deleteFile(fileId){
+        try {
+            return await this.storage.deleteFile(
+                conf.appWriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.log("delete file error.")
+            return false;
+        }
+    }
+
+    //file preview
+    getFilePreview(fileId){
+        return this.storage.getFilePreview(
+            conf.appWriteBucketId,
+            fileId
+        )
+    }
 }
 
 
